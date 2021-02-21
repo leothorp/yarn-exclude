aff2d732302e6b47a77f73ab129780cab4d8a100
leothorp <leothorp@users.noreply.github.com>
2021-02-21 00:44:25 -0600

    modify

--- index.js
+++ index.js
@@ -3,23 +3,42 @@
   const util = require("util");
   const cbGlob = require("glob");
   const fs = require("fs/promises");
+  const fs2 = require("fs");
   const path = require("path");
-
+  const { exec: cbExec, spawn } = require("child_process");
   const invariant = (cond, msg) => {
     if (!cond) {
       throw new Error(msg);
     }
   };
-  const glob = (path, opts) => {
-    return new Promise((res, rej) => {
-      cbGlob(path, opts, (err, files) => {
-        if (err) {
-          rej(err);
-        }
-        res(files);
+
+  //own implementation, to handle nonstandard structure of exec
+  const toPromise = (fn) => {
+    const wrappedFn = (...args) => {
+      return new Promise((res, rej) => {
+        console.log("prom", "in", ...args);
+
+        fn.call(null, ...args, (err, ...resultArgs) => {
+          console.log("cb");
+
+          if (err) {
+            console.error("err", err);
+            rej(err);
+            return;
+          }
+          console.log("prom", resultArgs);
+
+          if (resultArgs.length > 1) {
+            res(resultArgs);
+          } else {
+            res(resultArgs[0]);
+          }
+        });
       });
-    });
+    };
+    return wrappedFn;
   };
+  const glob = toPromise(cbGlob);
 
   const parsePkgJson = (pkgDir) => {
     const filePath = path.resolve(pkgDir, "package.json");
@@ -28,29 +47,33 @@
       .then((f) => JSON.parse(f));
   };
 
-  //yarn-exclude three --frozen-lockfile
   const program = new commander.Command();
   program.version("0.0.1");
   program
     .requiredOption("-e --exclude <excludePkg>", "excluded package")
-    .option(
-      "-d --dir <directory>",
-
-      "workspace root directory",
-      process.cwd()
+    .requiredOption(
+      "--modify",
+      "leave yarn.lock and package.json modifications in place. May be useful in a CI environment where yarn install is run multiple times."
     )
-    .allowUnknownOption(true);
+    .option("--cwd <directory>", "workspace root directory", process.cwd())
+
+    //TODO(leo): vvv
+    .allowUnknownOption(true); //for reg. yarn options
 
   program.parse(process.argv);
-  const { dir, exclude } = program.opts();
-  console.log(program.opts());
+  const { dir, exclude, modify } = program.opts();
+
   const resolveWith = (p) => path.resolve(dir, p);
 
   const pkgJsonPath = resolveWith("package.json");
-  const tmpPkgJsonPath = path.resolve(".", "temp-package.json");
+  console.log(pkgJsonPath);
+  const yarnLockPath = resolveWith("yarn.lock");
+  const tmpDir = await fs.mkdtemp("yarn-exclude-tmp");
+  const tmpPackageJsonPath = path.resolve(tmpDir, "package.json");
+  const tmpYarnLockPath = path.resolve(tmpDir, "yarn.lock");
 
   const packageJson = await parsePkgJson(dir);
-
+  console.log("yes", packageJson);
   invariant(
     !!packageJson.workspaces,
     `No 'workspaces' entry was found in package.json.`
@@ -60,26 +83,82 @@
     ? { packages: packageJson.workspaces, nohoist: [] }
     : packageJson.workspaces;
   const packagesArr = normalizedOrigWorkspacesVal.packages;
+  console.log("yes2", packageJson);
 
   const packageDirs = (
-    await Promise.all(packagesArr.map(resolveWith).map(glob))
+    await Promise.all(packagesArr.map(resolveWith).map((pkg) => glob(pkg, {})))
   ).flat();
+  console.log("yes3", packageJson);
 
   invariant(!!packageDirs.length, `No packages found.`);
 
-  const filtered = packageDirs.filter(
-    (w) => w !== exclude && path.basename(w) !== exclude
-  );
-  const tmpPackageJson = {
+  const filtered = packageDirs
+    .filter((w) => w !== exclude && path.basename(w) !== exclude)
+    .map((p) => path.relative(dir, p));
+  const updatedPackageJson = {
     ...packageJson,
     workspaces: { ...normalizedOrigWorkspacesVal, packages: filtered },
   };
 
-  console.log("tmp", tmpPackageJson);
+  console.log("tmp", updatedPackageJson);
+  console.log("tmp", tmpDir);
+  //TODO(leo): idea- dstructive operation safe rollback pkg for file work w Node?
 
-  await fs.writeFile(path, JSON.stringify(tmpPackageJson), {
-    encoding: "utf8",
-  });
+  await Promise.all([
+    fs.copyFile(pkgJsonPath, tmpPackageJsonPath),
+    //TODO(leo): revert below
+
+    fs.copyFile(yarnLockPath, tmpYarnLockPath),
+  ]);
+
+  const restoreFiles = async () => {
+    await Promise.all([
+      fs.copyFile(tmpPackageJsonPath, pkgJsonPath),
+      //TODO(leo): revert below
+      fs.copyFile(tmpYarnLockPath, yarnLockPath),
+    ]);
+
+    await fs.rmdir(tmpDir, { recursive: true });
+  };
+
+  try {
+    //write new package.json, run yarn
+    await fs.writeFile(
+      pkgJsonPath,
+      JSON.stringify(updatedPackageJson, null, 2)
+    );
+
+    //TODO(leo): vvv rm force
+    console.log(process.cwd());
+    const yarnProcess = spawn(
+      "yarn",
+      [
+        "install",
+        !modify && "--frozen-lockfile",
+        "--cwd",
+        dir,
+        ...program.args.filter((a) => a !== "--frozen-lockfile"),
+      ].filter((x) => x),
+      {
+        stdio: "inherit",
+      }
+    );
+
+    yarnProcess.on("exit", function (code) {
+      console.log("success.");
+      if (!modify) {
+        restoreFiles();
+      }
+
+      console.log("child process exited with code " + code.toString());
+    });
+  } catch (e) {
+    console.error(e);
+    console.log("restoring files");
+
+    await restoreFiles();
+    throw e;
+  }
 
   //make new pkg json
   //run yarn
